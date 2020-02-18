daa42940faa4af6712f8e98e2e9262106b2041d9
big <big@big-PC>
2020-02-18 11:57:23 +0900

    문자열 비교 추가

--- springweb/src/main/java/com/springweb/framework/util/StringUtil.java
+++ springweb/src/main/java/com/springweb/framework/util/StringUtil.java
@@ -12,6 +12,16 @@ public class StringUtil {
 	}
 
 	/**
+	 * 문자열이 null 이거나 blank 인지 체크
+	 * @param param 문자열
+	 * @return Boolean true/false
+	 */
+	public static boolean isNullOrBlank(Object arg)
+	{
+		return ( (arg == null) ? true: isNullOrBlank(String.valueOf(arg)));
+	}
+
+	/**
 	* 공백이나 null이 아닐 경우 문자열 리턴, 그외에는 null 리턴
 	* @param val
 	* @return
@@ -23,5 +33,19 @@ public class StringUtil {
 			result =String.valueOf(val);
 		}
 		return result;
+	}
+
+	/**
+	 * 같은 문자열인지 비교
+	 * @param val1
+	 * @param val2
+	 * @return Boolean true/false
+	 */
+	public static boolean isEqual(Object val1, Object val2){
+		if(StringUtil.isNullOrBlank(val1)) return false;
+		if(StringUtil.isNullOrBlank(val2)) return false;
+		if(String.valueOf(val1).equals(String.valueOf(val2))) return true;
+
+		return false;
 	}
 }
