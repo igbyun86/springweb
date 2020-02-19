8cf0726f46971c5af877d69cf3cc73e6cd559602
big <igbyun86@gmail.com>
2020-02-19 16:28:32 +0900

    main 추가

--- springweb/src/main/java/com/springweb/main/MainController.java
+++ springweb/src/main/java/com/springweb/main/MainController.java
@@ -1,9 +1,23 @@
 package com.springweb.main;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
+	@RequestMapping(method = RequestMethod.GET, value="/main")
+	public String main(@RequestParam Map<String,Object> paramMap, ModelMap model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return "/main/main";
+	}
 
 }
