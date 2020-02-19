e59a13701ba700fdf5222edaabafd6d7e261c625
big <igbyun86@gmail.com>
2020-02-19 16:28:04 +0900

    post 추가

--- springweb/src/main/java/com/springweb/main/LoginController.java
+++ springweb/src/main/java/com/springweb/main/LoginController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	protected final Logger log = LogManager.getLogger();
 
-	@RequestMapping(method = RequestMethod.GET, value="/login")
+	@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="/login")
 	public String main(@RequestParam Map<String,Object> paramMap, ModelMap model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		return "/main/login";
