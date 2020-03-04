ad335b9028a0881119343aa557bd8da702ee3977
big <igbyun86@gmail.com>
2020-03-04 21:11:48 +0900

    vue 예제 추가

---
+++ springweb/src/main/java/com/springweb/vue/VueController.java
@@ -0,0 +1,26 @@
+package com.springweb.vue;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class VueController {
+
+	@RequestMapping(method = RequestMethod.GET, value="/vue")
+	public String main(@RequestParam Map<String,Object> paramMap, ModelMap model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return "/vue/vue";
+	}
+
+
+
+
+}
